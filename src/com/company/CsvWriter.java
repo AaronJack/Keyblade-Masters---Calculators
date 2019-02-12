e6e032ab29ee6bc6d485fc2d9c4f15c59c1de0fc
Nicholas-Por <nporzio3890@bths.edu>
2019-02-11 19:24:41 -0500

    CSV works properly its fixed now

--- src/com/company/CsvWriter.java
+++ src/com/company/CsvWriter.java
@@ -3,18 +3,19 @@ import java.io.*;
 import java.util.ArrayList;
 import java.io.FileWriter;
 public class CsvWriter
-    //WRITES ON THE SAME LINE FIX IT SO IT STARTS A NEW LINE EACH TIME IT RUNS
+
 {
 
-    public static void problemLogger(String prob)   {
+    public static void problemLogger(String prob)
+
+    {
 
         try
         {
             FileWriter sb = new FileWriter("data.csv", true);
-            sb.append(prob);
             sb.append(System.getProperty("line.separator"));
             sb.write("Equation: ");
-            sb.write(sb.toString());
+            sb.write(prob.toString());
             sb.append('\n');
             sb.flush();
             sb.close();
@@ -29,10 +30,10 @@ public class CsvWriter
         try
         {
             FileWriter sb = new FileWriter("data.csv", true);
-            sb.append(sol);
+
             sb.append(System.getProperty("line.separator"));
             sb.write("Solution: ");
-            sb.write(sb.toString());
+            sb.write(sol.toString());
             sb.flush();
             sb.close();
         }
@@ -46,9 +47,9 @@ public class CsvWriter
         try
         {
             FileWriter sb = new FileWriter("data.csv", true);
-            sb.append(" User has cleared data ");
+            sb.write("User has cleared data");
             sb.append(System.getProperty("line.separator"));
-            sb.write(sb.toString());
+
             sb.flush();
             sb.close();
         }
